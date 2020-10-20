d3d9a7bf9323fb9f8e13a89c2394bdbe0df9ed4f
SandboxGTASA <55704051+SandboxGTASA@users.noreply.github.com>
2020-10-20 18:50:27 -0300

    att

--- Jogo da forca/jogo_da_forca.py
+++ Jogo da forca/jogo_da_forca.py
@@ -2,6 +2,7 @@ import random
 import time
 
 palavras = ('casa', 'abelha', 'cachueira', 'porta', 'mesa', 'lixeira', 'cadeado', 'guarda')
+palavras = ('casa', 'casa')
 sorteio_palavras = random.choice(palavras)  # Pegando palavras aleatorias
 letras = []
 tentantiva = 5
@@ -11,7 +12,7 @@ acertou = False
 print('='*20 + ' JOGO DA FORCA ' + '='*20 + '\n')
 
 print('Sorteando um palavra...')
-time.sleep(1.5)  # Criando um pequeno cronometro
+# time.sleep(1.5)  # Criando um pequeno cronometro
 print(f'A palavra sorteada possui {len(sorteio_palavras)} letras\n')
 
 # Fazendo um for para substiruir as letras sortiadas por traços
@@ -27,26 +28,32 @@ while acertou == False:
         if enter_letras == sorteio_palavras[i]:
             letras[i] = enter_letras
         print(letras[i])
-    acertou = True
+    # acertou = True
 
     # Pecorre a palavra sorteada e conpara se ainda existe algum taço
     # Caso tenha contunua o laço
-    for x in range(0, len(sorteio_palavras)):
-        if letras[x] == '-':
-            acertou = False
+    # for x in range(0, len(sorteio_palavras)):
+    #     if letras[x] == '-':
+    #         acertou = False
 
     # OBS -> Ao entra nessas linhas do codigo a intenção é que a cada letra errada o numero de
     # tentativas seja - 1, o que realmente acontece, porem tambem as tentativas estão sendo subtraidas
     # caso a letra estejam certas.
     if enter_letras == sorteio_palavras:
-        tentantiva = tentantiva
-    else:
-        print(f'Você possui {tentantiva} Tentativas.')
+    	print('acertou mizeravi',sorteio_palavras)
+    	acertou = True
+        # tentantiva = tentantiva
+    elif letras == list(sorteio_palavras):
+    	print('acertou mizeravi',sorteio_palavras)
+    	acertou = True
+    elif enter_letras not in sorteio_palavras:
         tentantiva -= 1
-
-    # Comparando as tentativas
-    if tentantiva <= 0:
-        print('enforcado')
-        print(letras)
-        break
-
+        print(f'Você possui {tentantiva} Tentativas.')
+        # Comparando as tentativas
+        if tentantiva <= 0:
+            print('enforcado')
+            print(sorteio_palavras)
+            break
+    # print(letras)
+    # print(sorteio_palavras)
+    # print(list(sorteio_palavras))
